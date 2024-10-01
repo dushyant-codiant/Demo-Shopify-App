c4bed2d500d1cef414b2877ca93560bf4a982db1
Dushyant Vishwakarma <dushyant.vishwakarma@codiant.com>
2024-10-01 15:17:40 +0530

    Non embedded app configured

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -12,6 +12,7 @@ const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.July24,
+  isEmbeddedApp: false,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
